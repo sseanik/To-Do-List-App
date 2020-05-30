431b0830c5bda61d046b732d410fdae8444da0ec
sseanik <seaniksmith@gmail.com>
2020-05-30 15:07:49 +1000

    Static Build

---
+++ frontend/build/precache-manifest.383cae728398672bfabd6091bfd69c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693775b24bed961d99370b9bd32e0f20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32df8abaa794f5b00bb5",
+    "url": "/static/css/main.a2ed811e.chunk.css"
+  },
+  {
+    "revision": "7d262e4fe4eed984c552",
+    "url": "/static/js/2.0960c821.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0960c821.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32df8abaa794f5b00bb5",
+    "url": "/static/js/main.1ce39031.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
