c3e512b85936f8a59b1ee8601d9d66acf1ca38cb
Sean Smith <seaniksmith@gmail.com>
2021-09-03 18:19:10 +1000

    Fixing build error

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ def getAutoID(task):
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists(f"{app.static_folder}/{path}"):
+    print(app.static_folder + "/" + path)
+    if path != "" and os.path.exists(app.static_folder + "/" + path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, "index.html")
